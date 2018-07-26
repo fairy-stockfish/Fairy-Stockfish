4f1bbeffc5986718ca4eebcc9866f2e51d869bae
ianfab <ianfab@users.noreply.github.com>
2018-07-26 22:22:44 +0200

    Divide piece score by 2 for drop variants
    STC
    LLR: 2.97 (-2.94,2.94) [0.00,10.00]
    Total: 302 W: 202 L: 96 D: 4
    
    LTC
    LLR: 2.95 (-2.94,2.94) [0.00,10.00]
    Total: 262 W: 182 L: 77 D: 3

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -903,7 +903,8 @@ namespace {
     // Initialize score by reading the incrementally updated scores included in
     // the position object (material + piece square tables) and the material
     // imbalance. Score is computed internally from the white point of view.
-    Score score = pos.psq_score() + me->imbalance() + pos.this_thread()->contempt;
+    Score score = pos.captures_to_hand() ? pos.psq_score() / 2 : pos.psq_score();
+    score += me->imbalance() + pos.this_thread()->contempt;
 
     // Probe the pawn hash table
     pe = Pawns::probe(pos);
