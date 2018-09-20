1c6c1fb01f2f80e0c658e0d8f77f92398279e9c9
Fabian Fichter <ianfab@users.noreply.github.com>
2018-09-20 22:08:15 +0200

    Tweak threat evaluation for antichess variants
    STC giveaway
    LLR: 2.95 (-2.94,2.94) [0.00,10.00]
    Total: 462 W: 226 L: 136 D: 100
    http://35.161.250.236:6543/tests/view/5ba3fe7a6e23db0fbab0ddaa
    
    STC losers
    LLR: 2.97 (-2.94,2.94) [0.00,10.00]
    Total: 334 W: 195 L: 99 D: 40
    http://35.161.250.236:6543/tests/view/5ba3fe8b6e23db0fbab0ddad

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -549,7 +549,7 @@ namespace {
             if (type_of(pos.piece_on(s)) != KING)
                 moves |= pos.moves_from(Us, type_of(pos.piece_on(s)), s);
         }
-        score += make_score(200, 200) * popcount(attackedBy[Them][ALL_PIECES] & moves & ~pos.pieces());
+        score += make_score(300, 300) * popcount(attackedBy[Them][ALL_PIECES] & moves & ~pos.pieces());
     }
 
     // Non-pawn enemies
