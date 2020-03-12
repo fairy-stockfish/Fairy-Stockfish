969cdbd370741381e9adfac084bdade5441064d2
Fabian Fichter <ianfab@users.noreply.github.com>
2020-03-12 21:09:51 +0100

    Tweak piece values for nCheck
    3check STC
    LLR: -2.97 (-2.94,2.94) [0.00,10.00]
    Total: 11258 W: 5234 L: 5169 D: 855
    http://www.variantfishtest.org:6543/tests/view/5e625bda6e23db46beba4f52
    
    3check LTC
    LLR: -2.96 (-2.94,2.94) [0.00,10.00]
    Total: 18691 W: 8667 L: 8495 D: 1529
    http://www.variantfishtest.org:6543/tests/view/5e6286876e23db46beba4f57
    
    5check STC
    LLR: -2.97 (-2.94,2.94) [0.00,10.00]
    Total: 11102 W: 4635 L: 4568 D: 1899
    http://www.variantfishtest.org:6543/tests/view/5e65f5776e23db46beba4f6d
    
    5check LTC
    LLR: -2.95 (-2.94,2.94) [0.00,10.00]
    Total: 12616 W: 5145 L: 5056 D: 2415
    http://www.variantfishtest.org:6543/tests/view/5e6539966e23db46beba4f6b

--- src/psqt.cpp
+++ src/psqt.cpp
@@ -158,6 +158,9 @@ void init(const Variant* v) {
       else if (v->twoBoards)
           score = make_score(mg_value(score) * 7000 / (7000 + mg_value(score)),
                              eg_value(score) * 7000 / (7000 + eg_value(score)));
+      else if (v->checkCounting)
+          score = make_score(mg_value(score) * (40000 + mg_value(score)) / 41000,
+                             eg_value(score) * (30000 + eg_value(score)) / 31000);
       else if (pt == strongestPiece)
               score += make_score(std::max(QueenValueMg - PieceValue[MG][pt], VALUE_ZERO) / 20,
                                   std::max(QueenValueEg - PieceValue[EG][pt], VALUE_ZERO) / 20);
