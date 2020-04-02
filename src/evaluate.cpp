b63d15a192ed381f243ed5c21f914d189b228bd7
Fabian Fichter <ianfab@users.noreply.github.com>
2020-04-02 20:03:28 +0200

    Increase extinction threat bonus
    extinction STC
    LLR: 2.95 (-2.94,2.94) [0.00,10.00]
    Total: 1551 W: 781 L: 663 D: 107
    http://www.variantfishtest.org:6543/tests/view/5e82e98d6e23db4f73614b14
    
    extinction LTC
    LLR: 2.97 (-2.94,2.94) [0.00,10.00]
    Total: 2133 W: 1020 L: 896 D: 217
    http://www.variantfishtest.org:6543/tests/view/5e8366546e23db4f73614b28
    
    kinglet STC
    LLR: -2.95 (-2.94,2.94) [0.00,10.00]
    Total: 7871 W: 3491 L: 3471 D: 909
    http://www.variantfishtest.org:6543/tests/view/5e82e99e6e23db4f73614b17
    
    kinglet LTC
    LLR: 2.97 (-2.94,2.94) [0.00,10.00]
    Total: 5977 W: 2718 L: 2541 D: 718
    http://www.variantfishtest.org:6543/tests/view/5e8393746e23db4f73614b2c
    
    threekings STC
    LLR: 2.97 (-2.94,2.94) [0.00,10.00]
    Total: 1670 W: 364 L: 285 D: 1021
    http://www.variantfishtest.org:6543/tests/view/5e82eece6e23db4f73614b23
    
    threekings LTC
    LLR: 2.95 (-2.94,2.94) [0.00,10.00]
    Total: 1614 W: 278 L: 206 D: 1130
    http://www.variantfishtest.org:6543/tests/view/5e8393626e23db4f73614b2a

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -644,7 +644,7 @@ namespace {
         {
             PieceType pt = type_of(pos.piece_on(pop_lsb(&bExt)));
             if (pos.extinction_piece_types().find(pt) != pos.extinction_piece_types().end())
-                score += make_score(300, 300) / std::max(pos.count_with_hand(Them, pt) - pos.extinction_piece_count(), 1);
+                score += make_score(500, 500) / std::max(pos.count_with_hand(Them, pt) - pos.extinction_piece_count(), 1);
         }
     }
 
