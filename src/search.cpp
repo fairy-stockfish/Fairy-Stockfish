e84c77ec50fa5412ab06188339aec9f959101ca2
Fabian Fichter <ianfab@users.noreply.github.com>
2020-10-24 11:37:10 +0200

    Tweak NMP depth for losing chess
    giveaway STC
    LLR: 2.95 (-2.94,2.94) [0.00,10.00]
    Total: 1259 W: 523 L: 424 D: 312
    http://www.variantfishtest.org:6543/tests/view/5f93432e6e23db221d9e9373
    
    giveaway LTC
    LLR: 2.95 (-2.94,2.94) [0.00,10.00]
    Total: 1092 W: 430 L: 337 D: 325
    http://www.variantfishtest.org:6543/tests/view/5f93603b6e23db221d9e9384

--- src/search.cpp
+++ src/search.cpp
@@ -903,7 +903,7 @@ namespace {
         assert(eval - beta >= 0);
 
         // Null move dynamic reduction based on depth and value
-        Depth R = (982 - 150 * !pos.checking_permitted() + 85 * depth) / 256 + std::min(int(eval - beta) / 192, 3);
+        Depth R = (982 - 200 * pos.must_capture() - 150 * !pos.checking_permitted() + 85 * depth) / 256 + std::min(int(eval - beta) / 192, 3);
 
         ss->currentMove = MOVE_NULL;
         ss->continuationHistory = &thisThread->continuationHistory[0][0][NO_PIECE][0];
