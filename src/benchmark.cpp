0f0322e4ad9d6e26889bfbc04d01863a9c30d417
Fabian Fichter <ianfab@users.noreply.github.com>
2020-11-15 15:03:01 +0100

    Simplify compile-time switch for NNUE embedding
    Disable default net by default instead of disabling Use NNUE.

--- src/benchmark.cpp
+++ src/benchmark.cpp
@@ -131,11 +131,7 @@ vector<string> setup_bench(const Position& current, istream& is) {
   string limit     = (is >> token) ? token : "13";
   string fenFile   = (is >> token) ? token : "default";
   string limitType = (is >> token) ? token : "depth";
-#ifdef USE_NNUE
   string evalType  = (is >> token) ? token : "mixed";
-#else
-  string evalType  = (is >> token) ? token : "classic";
-#endif
 
   go = limitType == "eval" ? "eval" : "go " + limitType + " " + limit;
 
@@ -189,11 +185,7 @@ vector<string> setup_bench(const Position& current, istream& is) {
           ++posCounter;
       }
 
-#ifdef USE_NNUE
   list.emplace_back("setoption name Use NNUE value true");
-#else
-  list.emplace_back("setoption name Use NNUE value false");
-#endif
 
   return list;
 }
