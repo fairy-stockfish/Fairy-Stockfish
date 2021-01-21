f4fd9d6727b72c926e51c56df1882e311869f452
Fabian Fichter <ianfab@users.noreply.github.com>
2021-01-21 18:51:37 +0100

    Reduce knight-pawn material imbalance for atomic
    atomic STC
    LLR: 2.96 (-2.94,2.94) [0.00,10.00]
    Total: 14756 W: 5451 L: 5175 D: 4130
    http://www.variantfishtest.org:6543/tests/view/600876446e23db077dd6610d
    
    atomic LTC
    LLR: 2.97 (-2.94,2.94) [0.00,10.00]
    Total: 3112 W: 1095 L: 978 D: 1039
    http://www.variantfishtest.org:6543/tests/view/60092b306e23db077dd66123

--- src/material.cpp
+++ src/material.cpp
@@ -121,6 +121,8 @@ namespace {
                    + QuadraticOurs[QUEEN][PAWN] * pieceCount[Us][QUEEN]) * pieceCount[Us][PAWN]
                 + make_score( mg_value(QuadraticOurs[KNIGHT][KNIGHT]),
                              -eg_value(QuadraticOurs[KNIGHT][KNIGHT])) * pieceCount[Us][KNIGHT] * pieceCount[Us][KNIGHT];
+    else if (pos.blast_on_capture())
+        bonus -= make_score(mg_value(QuadraticOurs[KNIGHT][PAWN]) * pieceCount[Us][KNIGHT] * pieceCount[Us][PAWN] / 2, 0);
     else if (pos.check_counting())
         bonus -= 2 * QuadraticOurs[PAWN][PAWN] * pieceCount[Us][PAWN] * pieceCount[Us][PAWN];
     else if (pos.captures_to_hand())
