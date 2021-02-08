29a3127a2db8a4c1f8b3676a9384cce110140aa3
Fabian Fichter <ianfab@users.noreply.github.com>
2021-02-08 22:50:15 +0100

    Remove redundant legality check
    No functional change.

--- src/search.cpp
+++ src/search.cpp
@@ -1115,7 +1115,7 @@ moves_loop: // When in check, search starts from here
       {
           // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold
           moveCountPruning = moveCount >= futility_move_count(improving, depth)
-                            || (pos.must_capture() && (moveCountPruning || (pos.capture(move) && pos.legal(move))));
+                            || (pos.must_capture() && (moveCountPruning || pos.capture(move)));
 
           // Reduced depth of the next LMR search
           int lmrDepth = std::max(newDepth - reduction(improving, depth, moveCount), 0);
