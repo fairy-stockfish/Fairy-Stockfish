1437fe3a1dcd3e33b98e26409ad0f3b3c94c0613
Niklas Fiekas <niklas.fiekas@backscattering.de>
2021-05-23 13:47:49 +0200

    Port wider UCI_Elo range from ddugovic/Stockfish (closes #316)
    The Skill Level option already allowed values from -20 to 20, but not via
    UCI_Elo. Expands the UCI_Elo range to linearly correspond to the entire range
    of levels.

--- src/search.cpp
+++ src/search.cpp
@@ -391,12 +391,9 @@ void Thread::search() {
 
   // Pick integer skill levels, but non-deterministically round up or down
   // such that the average integer skill corresponds to the input floating point one.
-  // UCI_Elo is converted to a suitable fractional skill level, using anchoring
-  // to CCRL Elo (goldfish 1.13 = 2000) and a fit through Ordo derived Elo
-  // for match (TC 60+0.6) results spanning a wide range of k values.
   PRNG rng(now());
   double floatLevel = Options["UCI_LimitStrength"] ?
-                      std::clamp(std::pow((Options["UCI_Elo"] - 1346.6) / 143.4, 1 / 0.806), 0.0, 20.0) :
+                      std::clamp((Options["UCI_Elo"] - 1500.0) / 75.0, -20.0, 20.0) :
                         double(Options["Skill Level"]);
   int intLevel = int(floatLevel) +
                  ((floatLevel - int(floatLevel)) * 1024 > rng.rand<unsigned>() % 1024  ? 1 : 0);
