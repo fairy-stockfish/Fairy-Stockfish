e07d887603d755a776df6a8d4c972ea25754cc3e
Michael Taktikos <36267869+mtaktikos@users.noreply.github.com>
2021-10-04 13:22:44 +0200

    Update types.h
    Better adjustment of piece value

--- src/types.h
+++ src/types.h
@@ -340,7 +340,7 @@ enum Value : int {
   VALUE_MATE_IN_MAX_PLY  =  VALUE_MATE - MAX_PLY,
   VALUE_MATED_IN_MAX_PLY = -VALUE_MATE_IN_MAX_PLY,
 
-  PawnValueMg   = 150,   PawnValueEg   = 240,
+  PawnValueMg   = 170,   PawnValueEg   = 270,
   KnightValueMg = 781,   KnightValueEg = 854,
   BishopValueMg = 825,   BishopValueEg = 915,
   RookValueMg   = 1276,  RookValueEg   = 1380,
