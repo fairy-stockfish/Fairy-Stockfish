6934c5dc3123804067d78571da87a4f902b3deac
Fabian Fichter <ianfab@users.noreply.github.com>
2023-01-14 18:29:38 +0100

    Bump pyffish version

--- src/pyffish.cpp
+++ src/pyffish.cpp
@@ -54,7 +54,7 @@ void buildPosition(Position& pos, StateListPtr& states, const char *variant, con
 }
 
 extern "C" PyObject* pyffish_version(PyObject* self) {
-    return Py_BuildValue("(iii)", 0, 0, 75);
+    return Py_BuildValue("(iii)", 0, 0, 77);
 }
 
 extern "C" PyObject* pyffish_info(PyObject* self) {
