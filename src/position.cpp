6a22e430864906b6f2beac77a51433809c6fdc01
RainRat <rainrat78@yahoo.ca>
2023-04-22 05:15:24 -0700

    Flagvariants (#638)

--- src/position.cpp
+++ src/position.cpp
@@ -2675,15 +2675,52 @@ bool Position::is_immediate_game_end(Value& result, int ply) const {
   // capture the flag
   if (   capture_the_flag_piece()
       && flag_move()
-      && (popcount(capture_the_flag(sideToMove) & pieces(sideToMove, capture_the_flag_piece()))>=flag_piece_count()))
+      && (
+           (popcount(capture_the_flag(sideToMove) & pieces(sideToMove, capture_the_flag_piece()))>=flag_piece_count()) // opponent has >= number of pieces needed to win
+           || //-or-
+           (
+             (flag_piece_blocked_win()) //flagPieceBlockedWin variant option true
+             && //-and-
+             (capture_the_flag(sideToMove) & pieces(sideToMove, capture_the_flag_piece())) //at least one piece in flag zone
+             && //-and-
+             !(capture_the_flag(sideToMove) & ~pieces()) //no empty squares in flag zone
+           )
+         )
+     )
   {
-      result =  (popcount(capture_the_flag(~sideToMove) & pieces(~sideToMove, capture_the_flag_piece()))>=flag_piece_count())
-              && sideToMove == WHITE ? VALUE_DRAW : mate_in(ply);
+      result =
+           (
+             (
+               (popcount(capture_the_flag(~sideToMove) & pieces(~sideToMove, capture_the_flag_piece()))>=flag_piece_count()) // you have >= number of pieces needed to win
+               || //-or-
+               (
+                 (flag_piece_blocked_win()) //flagPieceBlockedWin variant option true
+                 && //-and-
+                 (capture_the_flag(~sideToMove) & pieces(~sideToMove, capture_the_flag_piece())) //at least one piece in flag zone
+                 && //-and-
+                 !(capture_the_flag(~sideToMove) & ~pieces()) //no empty squares in flag zone
+               )
+             )
+             &&
+               (sideToMove == WHITE) //opponent is white
+           )
+           ? VALUE_DRAW : mate_in(ply); //then it's a draw, otherwise, win
       return true;
   }
   if (   capture_the_flag_piece()
-      && (!flag_move() || capture_the_flag_piece() == KING)
-      && (popcount(capture_the_flag(~sideToMove) & pieces(~sideToMove, capture_the_flag_piece()))>=flag_piece_count()) )
+      && (!flag_move() || capture_the_flag_piece() == KING) //if black doesn't get an extra move to draw, or flag piece is king,
+      && ( //-and-
+           (popcount(capture_the_flag(~sideToMove) & pieces(~sideToMove, capture_the_flag_piece()))>=flag_piece_count()) // you have >= number of pieces needed to win
+           || //-or-
+           (
+               (flag_piece_blocked_win()) //flagPieceBlockedWin variant option true
+             && //-and-
+               (capture_the_flag(~sideToMove) & pieces(~sideToMove, capture_the_flag_piece())) //at least one piece in flag zone
+             && //-and-
+               !(capture_the_flag(~sideToMove) & ~pieces()) //no empty squares in flag zone
+           )
+         )
+     )
   {
       bool gameEnd = true;
       // Check whether king can move to CTF zone
