016896abea252a413bfba4f6cb7dde3cc6de62a8
Dr Haddad <34623005+musketeerchess@users.noreply.github.com>
2023-12-19 21:09:07 +0100

    Update piece.cpp
    Added some Musketeer Chess Pieces among them Hawk, Unicorn, Spider, Fortress, Musketeer Elephant, Musketeer Cannon,

--- src/piece.cpp
+++ src/piece.cpp
@@ -224,6 +224,13 @@ void PieceMap::init(const Variant* v) {
   add(WAZIR, from_betza("W", "wazir"));
   add(COMMONER, from_betza("K", "commoner"));
   add(CENTAUR, from_betza("KN", "centaur"));
+  add(HAWK, from_betza("ADGH", "hawk")); /added some Musketeer Chess Pieces used in Musketeer Chess Variant
+  add(MUSKETEER_ELEPHANT, from_betza("FWDA", "musketeerElephant"));
+  add(UNICORN, from_betza("NC", "unicorn"));
+  add(MUSKETEER_CANNON, from_betza("FWDsN", "musketeerCannon")); /different from Xiangqi Cannon
+  add(SPIDER, from_betza("B2ND", "spider"));
+  add(LEOPARD, from_betza("F2N", "leopard"));
+  add(FORTRESS, from_betza("B3vND", "fortress"));
   add(KING, from_betza("K", "king"));
   // Add custom pieces
   for (PieceType pt = CUSTOM_PIECES; pt <= CUSTOM_PIECES_END; ++pt)
