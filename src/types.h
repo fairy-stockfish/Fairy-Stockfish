50adcffd957aaa2b4729409518549fc3107b9c33
yjf2002ghty <47345902+yjf2002ghty@users.noreply.github.com>
2024-04-29 20:02:17 +0800

    Fix stack overflow when depth is too deep (#780)

--- src/types.h
+++ src/types.h
@@ -227,13 +227,22 @@ typedef uint64_t Bitboard;
 constexpr int SQUARE_BITS = 6;
 #endif
 
+//When defined, move list will be stored in heap. Delete this if you want to use stack to store move list. Using stack can cause overflow (Segmentation Fault) when the search is too deep.
+#define USE_HEAP_INSTEAD_OF_STACK_FOR_MOVE_LIST
+
 #ifdef ALLVARS
 constexpr int MAX_MOVES = 8192;
-constexpr int MAX_PLY   = 60;
+#ifdef USE_HEAP_INSTEAD_OF_STACK_FOR_MOVE_LIST
+constexpr int MAX_PLY = 246;
+#else
+constexpr int MAX_PLY = 60;
+#endif
+/// endif USE_HEAP_INSTEAD_OF_STACK_FOR_MOVE_LIST
 #else
 constexpr int MAX_MOVES = 1024;
-constexpr int MAX_PLY   = 246;
+constexpr int MAX_PLY = 246;
 #endif
+/// endif ALLVARS
 
 /// A move needs 16 bits to be stored
 ///
