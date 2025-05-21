417ff504d90e5f8a4a6f7e9c0195eab42fc5dbed
gbtami <gbtami@gmail.com>
2025-05-21 12:11:31 +0200

    Bump versions

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ pyffish_module = Extension(
     sources=sources,
     extra_compile_args=args)
 
-setup(name="pyffish", version="0.0.87",
+setup(name="pyffish", version="0.0.88",
       description="Fairy-Stockfish Python wrapper",
       long_description=long_description,
       long_description_content_type="text/markdown",
