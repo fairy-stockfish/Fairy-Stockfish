6ef31c65dee5262a2ba6afb520248935e040e655
johndoknjas <jdoknjas@sfu.ca>
2025-08-25 16:06:42 -0700

    Revert "Undo changes, and only update the condition for the knight winning when it is the king to move."
    This reverts commit 608a6b20709013007016576264b8dd1e2110d13f.

--- src/endgame.cpp
+++ src/endgame.cpp
@@ -1041,16 +1041,17 @@ Value Endgame<KN, EG_EVAL_ANTI>::operator()(const Position& pos) const {
 
   Square KSq = pos.square<COMMONER>(strongSide);
   Square NSq = pos.square<KNIGHT>(weakSide);
+  Bitboard kingAttacks = attacks_bb<KING>(KSq);
+  Bitboard knightAttacks = attacks_bb<KNIGHT>(NSq);
+  bool strongSideToMove = pos.side_to_move() == strongSide;
 
-  // wins for knight
-  if (pos.side_to_move() == strongSide && (attacks_bb<KNIGHT>(NSq) & KSq) && !(attacks_bb<KNIGHT>(NSq) & attacks_bb<KING>(KSq)))
-      return -VALUE_KNOWN_WIN;
-  if (pos.side_to_move() == weakSide && (attacks_bb<KNIGHT>(NSq) & attacks_bb<KING>(KSq)))
-      return VALUE_KNOWN_WIN;
+  if (strongSideToMove ? kingAttacks & NSq : knightAttacks & KSq) return mated_in(1);
+  if (kingAttacks & knightAttacks) return mate_in(2);
+  if (strongSideToMove ? knightAttacks & KSq : kingAttacks & NSq) return mated_in(3);
 
   Value result = VALUE_KNOWN_WIN + push_to_edge(NSq, pos) - push_to_edge(KSq, pos);
 
-  return strongSide == pos.side_to_move() ? result : -result;
+  return strongSideToMove ? result : -result;
 }
 
 /// N vs N. The side to move always wins/loses if the knights are on
