8e74db0cd4c4b285c6a22f4d7088438c19a29611
David <davidjsmurf@gmail.com>
2025-09-05 14:02:43 +0200

    feat: add iron pieces support (Issue #606)
    Add Variant::ironPieceTypes and parser support; disallow capturing iron pieces (incl. en passant); document key and demo variant [iron-pawns:chess].

--- src/variant.h
+++ src/variant.h
@@ -66,6 +66,8 @@ struct Variant {
   bool blastOnCapture = false;
   PieceSet blastImmuneTypes = NO_PIECE_SET;
   PieceSet mutuallyImmuneTypes = NO_PIECE_SET;
+  // Iron pieces: attempts to capture these piece types are illegal
+  PieceSet ironPieceTypes = NO_PIECE_SET;
   PieceSet petrifyOnCaptureTypes = NO_PIECE_SET;
   bool petrifyBlastPieces = false;
   bool doubleStep = true;
