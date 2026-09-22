6165162febf641a223c37a4e425d0dcc540e1288
Chris <chris@example.com>
2026-09-22 13:36:44 -0700

    Fix safe-piece counting for flag wins

--- src/position.h
+++ src/position.h
@@ -1076,12 +1076,16 @@ inline bool Position::flag_reached(Color c) const {
     {
         Bitboard piecesInFlagZone = flag_region(c) & pieces(c, flag_piece(c));
         int      potentialPieces  = popcount(piecesInFlagZone);
-        // If we are exactly at the required piece count, all pieces in the flag zone need to be safe
-        while (piecesInFlagZone && potentialPieces == var->flagPieceCount)
+        // Extra pieces may be unsafe as long as enough safe pieces remain
+        if (potentialPieces >= var->flagPieceCount)
         {
-            Square   sr            = pop_lsb(piecesInFlagZone);
-            Bitboard flagAttackers = attackers_to(sr, ~c);
-            if (flagAttackers)
+            while (piecesInFlagZone)
+            {
+                Square sr = pop_lsb(piecesInFlagZone);
+                if (attackers_to(sr, ~c))
+                    --potentialPieces;
+            }
+            if (potentialPieces < var->flagPieceCount)
                 return false;
         }
     }
